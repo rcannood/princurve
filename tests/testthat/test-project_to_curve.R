9b1c7bcccd6570e52568c4f74176338e54b73ae5
Robrecht Cannoodt <brechtus@gmail.com>
2018-07-03 10:14:12 +0200

    extend test

--- tests/testthat/test-project_to_curve.R
+++ tests/testthat/test-project_to_curve.R
@@ -3,6 +3,7 @@ context("Testing project_to_curve")
 z <- seq(-1, 1, length.out = 100)
 s <- cbind(z, z^2, z^3, z^4)
 x <- s + rnorm(length(s), mean = 0, sd = .005)
+ord <- sample.int(nrow(x))
 
 test_that("Testing project_to_curve", {
   lam <- project_to_curve(
@@ -31,4 +32,43 @@ test_that("Testing get.lam for backwards compatibility", {
 
   expect_gte(cor(as.vector(lam$s), as.vector(s)), .99)
   expect_gte(cor(lam$tag, seq_len(100)), .99)
+})
+
+
+test_that("Testing project_to_curve with shuffled order", {
+  lam <- project_to_curve(
+    x = x[ord,],
+    s = s,
+    stretch = 0
+  )
+
+  expect_gte(cor(as.vector(lam$s[lam$ord,]), as.vector(s)), .99)
+  expect_gte(cor(order(lam$ord), ord), .99)
+})
+
+test_that("Values are more or less correct", {
+  constant_s <- matrix(c(-1, 1, .1, .1, .2, .2, .3, .3), nrow = 2, byrow = FALSE)
+  x[,1] <- z
+
+  lam <- project_to_curve(
+    x = x,
+    s = constant_s,
+    stretch = 0
+  )
+
+  expect_true(all(abs(lam$s[,1] - x[,1]) < 1e-6))
+  expect_true(all(abs(lam$s[,2] - .1) < 1e-6))
+  expect_true(all(abs(lam$s[,3] - .2) < 1e-6))
+  expect_true(all(abs(lam$s[,4] - .3) < 1e-6))
+  expect_equal(lam$ord, seq_along(z))
+  expect_true(all(abs(lam$lambda - seq(0, 2, length.out = 100)) < 1e-6))
+
+  dist_ind <-
+    (x[,2] - .1)^2 +
+    (x[,3] - .2)^2 +
+    (x[,4] - .3)^2
+
+  expect_true(all(abs(lam$dist_ind - dist_ind) < 1e-10))
+
+  expect_true(abs(sum(dist_ind) - lam$dist) < 1e-10)
 })

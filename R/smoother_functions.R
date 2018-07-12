c356c861aaa03f15158ce090cd3243fa1fd8f838
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-12 15:05:27 +0200

    add documentation for the smoother functions

--- R/smoother_functions.R
+++ R/smoother_functions.R
@@ -1,3 +1,15 @@
+#' Smoother functions
+#'
+#' Each of these functions have an interface \code{function(lambda, xj, ...)}, and
+#' return smoothed values for xj. The output is expected to be ordered along an ordered lambda.
+#' This means that the following is true:
+#' \preformatted{
+#' x <- runif(100)
+#' y <- runif(100)
+#' ord <- sample.int(100)
+#' sfun <- smoother_functions[[1]]
+#' all(sfun(x, y) == sfun(x[ord], y[ord]))
+#' }
 smoother_functions <- list(
   smooth_spline = function(lambda, xj, ..., df = 5) {
     ord <- order(lambda)

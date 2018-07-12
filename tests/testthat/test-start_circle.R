1dc2203c8bd66c0f04eb54d75c787f2eace9b00e
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-12 13:00:59 +0200

    test whether start_circle produces correct output after translation or scaling

--- tests/testthat/test-start_circle.R
+++ tests/testthat/test-start_circle.R
@@ -1,20 +1,35 @@
 context("Testing start_circle")
 
-test_start_circle_output <- function(x_orig, fit) {
+test_start_circle_output <- function(x_orig) {
+  xbar_orig <- apply(x_orig, 2, mean)
+
+  fit <- start_circle(x_orig)
   x_new <- fit$s
+  xbar_new <- apply(x_new, 2, mean)
+  radius_new <- colMeans(abs(sweep(x_new, 2, xbar_new)))[1:2]
 
   expect_equal(dim(x_new), dim(x_orig))
   expect_equal(rownames(x_new), rownames(x_orig))
   expect_equal(colnames(x_new), colnames(x_orig))
-
-  xbar_orig <- apply(x_orig, 2, mean)
-  radius_orig <- colMeans(abs(sweep(x_orig, 2, xbar_orig)))
-
-  xbar_new <- apply(x_new, 2, mean)
-  radius_new <- colMeans(abs(sweep(x_new, 2, xbar_new)))
-
   expect_lte(sum((xbar_orig - xbar_new)^2), 1e-2)
-  expect_lte(sum((radius_orig[1:2] - radius_new[1:2])^2), 1e-2)
+
+  # check whether bar and radius update correctly after translation
+  fit2 <- start_circle(x_orig + 10)
+  x_new2 <- fit2$s
+  xbar_new2 <- apply(x_new2, 2, mean)
+  radius_new2 <- colMeans(abs(sweep(x_new2, 2, xbar_new2)))[1:2]
+
+  expect_lte(sum((abs(xbar_new2 - xbar_new) - 10)^2), 1e-10)
+  expect_lte(sum((radius_new2 - radius_new)^2), 1e-10)
+
+  # check whether bar and radius update correctly after scaling
+  fit3 <- start_circle(x_orig * 10)
+  x_new3 <- fit3$s
+  xbar_new3 <- apply(x_new3, 2, mean)
+  radius_new3 <- colMeans(abs(sweep(x_new3, 2, xbar_new3)))[1:2]
+
+  expect_lte(sum((abs(xbar_new3 / xbar_new) - 10)^2), 1e-10)
+  expect_lte(sum((abs(radius_new3 / radius_new) - 10)^2), 1e-10)
 }
 
 test_that("Testing start_circle", {
@@ -23,20 +38,16 @@ test_that("Testing start_circle", {
     rnorm(100, 0, 1)
   )
 
-  fit <- start_circle(x_orig)
-  test_start_circle_output(x_orig, fit)
+  test_start_circle_output(x_orig)
 
   colnames(x_orig) <- paste0("Dim", seq_len(ncol(x_orig)))
-  fit <- start_circle(x_orig)
-  test_start_circle_output(x_orig, fit)
+  test_start_circle_output(x_orig)
 
   rownames(x_orig) <- paste0("Sample", seq_len(nrow(x_orig)))
-  fit <- start_circle(x_orig)
-  test_start_circle_output(x_orig, fit)
+  test_start_circle_output(x_orig)
 
   colnames(x_orig) <- NULL
-  fit <- start_circle(x_orig)
-  test_start_circle_output(x_orig, fit)
+  test_start_circle_output(x_orig)
 })
 
 
@@ -48,19 +59,15 @@ test_that("Testing start_circle", {
     runif(404, 2.9, 3.1)
   )
 
-  fit <- start_circle(x_orig)
-  test_start_circle_output(x_orig, fit)
+  test_start_circle_output(x_orig)
 
   colnames(x_orig) <- paste0("Dim", seq_len(ncol(x_orig)))
-  fit <- start_circle(x_orig)
-  test_start_circle_output(x_orig, fit)
+  test_start_circle_output(x_orig)
 
   rownames(x_orig) <- paste0("Sample", seq_len(nrow(x_orig)))
-  fit <- start_circle(x_orig)
-  test_start_circle_output(x_orig, fit)
+  test_start_circle_output(x_orig)
 
   colnames(x_orig) <- NULL
-  fit <- start_circle(x_orig)
-  test_start_circle_output(x_orig, fit)
+  test_start_circle_output(x_orig)
 })
 

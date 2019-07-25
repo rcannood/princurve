d00841de436ab2d9bc1913d6fff1eb7746a7f1b6
Robrecht Cannoodt <rcannood@gmail.com>
2019-07-25 10:51:28 +0200

    use expect_equivalent with tolerances in legacy comparison testthat

--- tests/testthat/test-legacy_princurve.R
+++ tests/testthat/test-legacy_princurve.R
@@ -1,4 +1,4 @@
-context("Comparing principal.curve and get.lam to legacy package")
+context("Comparing principal_curve and project_to_curve to legacy package")
 
 skip_on_cran()
 
@@ -8,33 +8,33 @@ if (!already_installed) {
 }
 
 for (i in seq_len(10)) {
-  test_that(paste0("Directly compare principal.curve against legagy, run ", i), {
+  test_that(paste0("Directly compare principal_curve against legagy, run ", i), {
     x <- matrix(runif(1000), ncol = 10)
 
     fit1 <- principal_curve(x)
     fit2 <- princurvelegacy::principal.curve(x)
 
-    expect_gte(abs(cor(as.vector(fit1$s), as.vector(fit2$s))), .99)
+    expect_equivalent(fit1$s, fit2$s, tolerance = .001)
     expect_gte(cor(order(fit1$ord), order(fit2$tag)), .99)
-    expect_gte(abs(cor(fit1$lambda, fit2$lambda)), .99)
-    expect_lte(abs(fit1$dist - fit2$dist), .01)
+    expect_equivalent(fit1$lambda, fit2$lambda, tolerance = .001)
+    expect_equivalent(fit1$dist, fit2$dist, tolerance = .001)
     expect_equal(fit1$converged, fit2$converged)
     expect_equal(fit1$num_iterations, fit2$nbrOfIterations)
   })
 }
 
 for (i in seq_len(10)) {
-  test_that(paste0("Directly compare get.lam against legagy, run ", i), {
+  test_that(paste0("Directly compare project_to_curve against legagy, run ", i), {
     x <- matrix(runif(1000), ncol = 10)
     s <- matrix(runif(100), ncol = 10)
 
     fit1 <- project_to_curve(x, s)
     fit2 <- princurvelegacy::get.lam(x, s)
 
-    expect_gte(abs(cor(as.vector(fit1$s), as.vector(fit2$s))), .99)
+    expect_equivalent(fit1$s, fit2$s, tolerance = .001)
     expect_gte(cor(order(fit1$ord), order(fit2$tag)), .99)
-    expect_gte(abs(cor(fit1$lambda, fit2$lambda)), .99)
-    expect_lte(abs(fit1$dist - fit2$dist), .01)
+    expect_equivalent(fit1$lambda, fit2$lambda, tolerance = .001)
+    expect_equivalent(fit1$dist, fit2$dist, tolerance = .001)
   })
 }
 
